ab96b5e3894e4461c17ca2528fe40fccafa2a935
Sebastien Cagnon <cagnonsebastien@gmail.com>
2017-01-20 11:24:03 +0900

    created Nao's AI, closes #4

--- SBTGames/html/js/tickTackToe.js
+++ SBTGames/html/js/tickTackToe.js
@@ -6,6 +6,13 @@ var gameGrid = [
     [0,0,0]
 ];
 
+// default score grid
+var scoreGrid = [
+    [1,0,1],
+    [0,2,0],
+    [1,0,1]
+];
+
 var texts = {
     "youStart": "You start!",
     "naoStart": "Nao starts!",
@@ -15,6 +22,11 @@ var texts = {
 
 var whoseTurn = "user";
 var started = false;
+
+// difficulty is the change of a non-random move
+// 0 means Nao plays at random
+// 1 means Nao always follow the best move (not perfect though)
+var difficulty = 0.9;
 
 function getLine(index) {
     return gameGrid[index];
@@ -59,11 +71,26 @@ function checkWin() {
     }
 }
 
+function checkFull() {
+    for (var i=0; i<3; i++) {
+        for (var j=0; j<3; j++) {
+            if (gameGrid[i][j] == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 function nextTurn() {
     if (started) {
         if (checkWin() != undefined) {
             console.log("someone won: " + whoseTurn);
             console.log(checkWin());
+            started = false;
+            return;
+        } else if (checkFull()) {
+            console.log("draw!");
             started = false;
             return;
         }
@@ -92,16 +119,59 @@ function naoMove() {
     for (var i=0; i<3; i++) {
         for (var j=0; j<3; j++) {
             if (gameGrid[i][j] == 0) {
-                possibleMoves.push([i,j]);
+                var score = scoreMove(i, j);
+                possibleMoves.push([[i,j], score]);
             }
         }
     }
-    // random accross available space
-    var move = possibleMoves[randRange(0, possibleMoves.length)];
-    gameGrid[move[0]][move[1]] = 2;
-    var id = "#" + move.join("");
+    possibleMoves.sort(function sortByScore (a, b) {
+        return b[1] - a[1];
+    });
+    if (Math.random() < difficulty) {
+        console.log("chosen move");
+        var move = possibleMoves[0];
+    } else {
+        console.log("random move");
+        var move = possibleMoves[randRange(0, possibleMoves.length)];
+    }
+    gameGrid[move[0][0]][move[0][1]] = 2;
+    var id = "#" + move[0].join("");
     $(id).html('<img src="img/cross.png" />');
     nextTurn();
+}
+
+function scoreMove (i, j) {
+    var score = scoreGrid[i][j];
+    score += lineScore(getLine(i));
+    score += lineScore(getColumn(j));
+    if (i == j) {
+        score += lineScore(getDiagonal(0));
+    }
+    if (i+j == 2) {
+        score += lineScore(getDiagonal(1));
+    }
+    return score;
+}
+
+function lineScore (line) {
+    var circles = 0;
+    var crosses = 0;
+    for (var i=0; i<3; i++) {
+        if (line[i] == 1) {
+            circles++;
+        } else if (line[i] == 2) {
+            crosses++;
+        }
+    }
+    if (crosses == 2) {
+        return 200;
+    } else if (circles == 2) {
+        return 30;
+    } else if (crosses == 1 && circles == 0) {
+        return 5;
+    } else {
+        return 0;
+    }
 }
 
 function userClick (evt) {
@@ -112,16 +182,12 @@ function userClick (evt) {
     var id = $target.attr("id");
     var i = parseInt(id[0]);
     var j = parseInt(id[1]);
-    gameGrid[i][j]++;
-    gameGrid[i][j] %= 3;
     if (gameGrid[i][j]==0) {
+        gameGrid[i][j] = 1;
         $target.empty();
-    } else if (gameGrid[i][j]==1) {
         $target.html('<img src="img/circle.png" />');
-    } else if (gameGrid[i][j]==2) {
-        $target.html('<img src="img/cross.png" />');
+        nextTurn();
     }
-    nextTurn();
 }
 
 $(function () {
